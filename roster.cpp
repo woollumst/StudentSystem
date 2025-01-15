e34b6163cb8a00ab4a29f4ed2908a7bdc1cd92e2
Toby <woollumst@gmail.com>
2025-01-15 09:21:23 -0700

    Update roster.cpp

--- roster.cpp
+++ roster.cpp
@@ -1,28 +1,28 @@
 #include "roster.h";
 
-ClassRoster::ClassRoster() {
+Roster::Roster() {
 	
 }
 
 // classRosterArray : an array of pointers to hold data from studentDataTable
 // parse studentDataTable to create student object for each student in table, populate to classRosterArray
 
-void ClassRoster::add(string studentID, string firstName, string lastName, string emailAddress, int age, int daysInCourse1, int daysInCourse2, int daysInCourse3, DegreeProgram degreeProgram) {
+void Roster::add(string studentID, string firstName, string lastName, string emailAddress, int age, int daysInCourse1, int daysInCourse2, int daysInCourse3, DegreeProgram degreeProgram) {
 	Student newStudent = new Student;
 }
-void ClassRoster::remove(string studentID) {
+void Roster::remove(string studentID) {
 	
 }
-void ClassRoster::printAll() {
+void Roster::printAll() {
 	// A1 [tab] First Name: John [tab] Last Name: Smith [tab] Age: 20 [tab]daysInCourse: {35, 40, 55} Degree Program: Security.
 	
 }
-void ClassRoster::printAverageDaysInCourse(string studentID) {
+void Roster::printAverageDaysInCourse(string studentID) {
 
 }
-void ClassRoster::printInvalidEmails() {
+void Roster::printInvalidEmails() {
 
 }
-void ClassRoster::printByDegreeProgram(DegreeProgram degreeProgram) {
+void Roster::printByDegreeProgram(DegreeProgram degreeProgram) {
 
 }
