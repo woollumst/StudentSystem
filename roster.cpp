368ee6363dbd4f6efdeb7dc46d94782afec029f0
Toby <woollumst@gmail.com>
2025-01-15 09:29:58 -0700

    Update roster.cpp

--- roster.cpp
+++ roster.cpp
@@ -8,21 +8,26 @@ Roster::Roster() {
 // parse studentDataTable to create student object for each student in table, populate to classRosterArray
 
 void Roster::add(string studentID, string firstName, string lastName, string emailAddress, int age, int daysInCourse1, int daysInCourse2, int daysInCourse3, DegreeProgram degreeProgram) {
-	Student newStudent = new Student;
+   
 }
 void Roster::remove(string studentID) {
 	
 }
 void Roster::printAll() {
 	// A1 [tab] First Name: John [tab] Last Name: Smith [tab] Age: 20 [tab]daysInCourse: {35, 40, 55} Degree Program: Security.
-	
+   for (Student student : Roster){
+      student.print();
+   }
 }
 void Roster::printAverageDaysInCourse(string studentID) {
-
+   //find student from student ID, print average days in courses
 }
 void Roster::printInvalidEmails() {
-
+   //add email validation
 }
 void Roster::printByDegreeProgram(DegreeProgram degreeProgram) {
-
+   for(Student student : Roster){
+      if(student.getDegreeProgram() == degreeProgram)
+         student.print();
+   }
 }
