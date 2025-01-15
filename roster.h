db30a6f0d2680afc1ed54d803a7cafc8891b1baf
Toby <woollumst@gmail.com>
2025-01-15 08:30:57 -0700

    Update roster.h

--- roster.h
+++ roster.h
@@ -5,7 +5,7 @@ using namespace std;
 
 class ClassRoster {
 	// classRosterArray : an array of pointers to hold data from studentDataTable
-	Student* classRosterArray[5]; // ?? Prob needs work. Pointer array?
+	Student* classRosterArray = new Student[]; // ?? Prob needs work. Pointer array?
 	
 	// parse studentDataTable to create student object for each student in table, populate to classRosterArray
 
